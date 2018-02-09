279916669570f6c57368ec67dafb560c0c3fd0ab
Sajid Hasan <sajidhasanapon@gmail.com>
2018-02-09 23:31:52 +0600

    Added crash recovery in Email Notifier

--- Email Notifier (Windows)/email_notifier.py
+++ Email Notifier (Windows)/email_notifier.py
@@ -2,19 +2,29 @@
 from imaplib import IMAP4_SSL
 from subprocess import call
 from time import sleep
-def main():
-	sleep(20)
 
-	obj = IMAP4_SSL('imap.gmail.com','993')
-	obj.login('username','password')
-	obj.select()
+def main():
 	
+	sleep(20)
+	login_success = False
+	
+	obj = IMAP4_SSL('imap.gmail.com','993')
+	while login_success == False:
+		try:
+			obj.login('username','password')
+			obj.select()
+			login_success = True
+		except:
+			login_success = False
+			
 	while True:
-		n = len(obj.search(None,'NOT SEEN')[1][0].split())
-		if n > 0:
-			call(["start", "email_notifier.bat"], shell=True)
-		sleep(300)
-		
+		try:
+			n = len(obj.search(None,'NOT SEEN')[1][0].split())
+			if n > 0:
+				call(["start", "email_notifier.bat"], shell=True)
+				sleep(300)
+		except:
+			None
 
 
 if __name__ == "__main__":
